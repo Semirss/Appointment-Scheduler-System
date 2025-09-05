8c34799952a81e1eb30d859839f51c0d0d747bc6
Semir_S <149691609+Semirss@users.noreply.github.com>
2025-09-05 07:33:54 +0300

    h

--- appointment-scheduler-system/vite.config.js
+++ appointment-scheduler-system/vite.config.js
@@ -4,12 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  build: {
-    outDir: 'dist',
-  },
-  base: './', // This is important for proper routing
+  plugins: [ react(),tailwindcss(),],
 })
